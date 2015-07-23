6cf6a8579e287d3b6a72b6e8c204bbc12be3e26e
says33 <says.rodriguez@gmail.com>
2015-07-23 11:59:59 -0500

    #1 se ajusta el modelo para agregar propiedades a las variables como unique,nullable, y se agrega la variable de Role

--- src/main/groovy/com/mx/User.groovy
+++ src/main/groovy/com/mx/User.groovy
@@ -8,9 +8,17 @@ class User {
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
   Long id
+  @Column(unique = true, nullable = false)
   String username
+  @Column(nullable = false)
   String password
+  @Column(nullable = false)
   String firstName
+  @Column(nullable = false)
   String lastName
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  Role role = Role.USER
+
 
 }
