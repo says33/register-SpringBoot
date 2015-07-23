4494c8beae7f496144661e0b900b37659f20a8e7
says33 <says.rodriguez@gmail.com>
2015-07-23 16:41:22 -0500

    #1 se eliminan por acomodo en sus carpetas deacuerdo a lo que son controller,domain y vistas

--- src/main/groovy/com/mx/Role.groovy
+++
@@ -1,5 +0,0 @@
-package com.mx
-
-enum Role {
-  USER,ADMIN
-}
--- src/main/groovy/com/mx/UserRepository.groovy
+++
@@ -1,7 +0,0 @@
- package com.mx
-
-import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.data.repository.CrudRepository
-
-interface UserRepository extends PagingAndSortingRepository<User,Long> {
-}
