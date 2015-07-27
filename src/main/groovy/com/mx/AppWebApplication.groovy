fce74ba92168b64bdee007ed654951eb7781a39e
says33 <says.rodriguez@gmail.com>
2015-07-27 11:21:56 -0500

    #1 se agrega el metod configure de springApplicationBuilder

--- src/main/groovy/com/mx/AppWebApplication.groovy
+++ src/main/groovy/com/mx/AppWebApplication.groovy
@@ -2,11 +2,17 @@ package com.mx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
 
 @SpringBootApplication
-public class AppWebApplication {
+public class AppWebApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(AppWebApplication.class, args);
+    }
+
+    SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      application.source(AppWebApplication.class)
     }
 }
