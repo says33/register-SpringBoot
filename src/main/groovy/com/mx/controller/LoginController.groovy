b1405c452745f3dacb83f453e2fc0de034f65ebf
says33 <says.rodriguez@gmail.com>
2015-07-28 12:13:26 -0500

    coding #2 se agrega logica con el model an view para agregar los errores solo cuando estos existan

--- src/main/groovy/com/mx/controller/LoginController.groovy
+++ src/main/groovy/com/mx/controller/LoginController.groovy
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.stereotype.Controller
 import org.springframework.web.servlet.ModelAndView
+import java.util.Optional
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -17,8 +18,12 @@ class LoginController {
   static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class)
 
   @RequestMapping(value="/login", method=RequestMethod.GET)
-  ModelAndView index(@RequestParam Collection<String> error){
-  	new ModelAndView("login/index", "error", error)
+  ModelAndView index(@RequestParam Optional<String> error){
+  	//new ModelAndView("login/index", "error", error)
+  	ModelAndView modelAndView = new ModelAndView("login/index")
+  	if (error)
+  		modelAndView.addObject("error", error)
+  	modelAndView
   }
 
 }
