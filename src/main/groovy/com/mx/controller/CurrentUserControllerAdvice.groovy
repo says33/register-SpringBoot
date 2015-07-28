0a02c2b93217e6491a556438bd9db6e013e1ac40
says33 <says.rodriguez@gmail.com>
2015-07-27 21:22:24 -0500

    #1 se agregan dependencias faltantes en esta clase

--- src/main/groovy/com/mx/controller/CurrentUserControllerAdvice.groovy
+++ src/main/groovy/com/mx/controller/CurrentUserControllerAdvice.groovy
@@ -1,9 +1,11 @@
 package com.mx.controller
 
 import com.mx.domain.CurrentUser
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.RequestMapping
+
 
 @ControllerAdvice
 class CurrentUserControllerAdvice {
@@ -12,5 +14,6 @@ class CurrentUserControllerAdvice {
   CurrentUser getCurrentUser(Authentication authentication) {
     (authentication == null) ? null : (CurrentUser) authentication.getPrincipal()
   }
+
 
 }
