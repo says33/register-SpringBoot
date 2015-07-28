4c715acf343c05fb2b2fa4f47a8df94769326bc6
says33 <says.rodriguez@gmail.com>
2015-07-28 12:12:15 -0500

    coding #2 se ajusta el operador ternario para el metodo getCurrentUser

--- src/main/groovy/com/mx/controller/CurrentUserControllerAdvice.groovy
+++ src/main/groovy/com/mx/controller/CurrentUserControllerAdvice.groovy
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 
-
 @ControllerAdvice
 class CurrentUserControllerAdvice {
   
   @ModelAttribute("currentUser")
   CurrentUser getCurrentUser(Authentication authentication) {
-    (authentication == null) ? null : (CurrentUser) authentication.getPrincipal()
+    authentication ? null :  (CurrentUser) authentication?.getPrincipal()
   }
 
 
